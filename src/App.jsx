920d7f21b1e41ab3d65c6ff33ae8bd5b7eb0fd59
Arsh-03 <mohammedarshkotwal@gmail.com>
2026-03-25 11:41:19 +0530

    Initial commit for Quiz App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react'
+import Quiz from './components/quiz'
+const App = () => {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+}
+
+export default App
